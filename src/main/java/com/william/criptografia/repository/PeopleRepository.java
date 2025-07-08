01cdc24f076bffea554de7ebf25d2f68004aed06
bobonimo111 <carlosmaycon443@gmail.com>
2025-07-08 17:40:37 -0300

    Added annotation

--- src/main/java/com/william/criptografia/repository/PeopleRepository.java
+++ src/main/java/com/william/criptografia/repository/PeopleRepository.java
@@ -2,8 +2,9 @@ package com.william.criptografia.repository;
 
 import com.william.criptografia.entity.PeopleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
-
+@Repository
 public interface PeopleRepository extends JpaRepository<PeopleEntity, UUID> {
 }
