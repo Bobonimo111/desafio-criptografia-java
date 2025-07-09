602ff8936241ffcbdc0b773e17594b1add02ee77
bobonimo111 <carlosmaycon443@gmail.com>
2025-07-09 14:14:05 -0300

    adição de query com optional

--- src/main/java/com/william/criptografia/repository/PeopleRepository.java
+++ src/main/java/com/william/criptografia/repository/PeopleRepository.java
@@ -4,7 +4,10 @@ import com.william.criptografia.entity.PeopleEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
+
 @Repository
 public interface PeopleRepository extends JpaRepository<PeopleEntity, UUID> {
+    Optional<PeopleEntity> findById(UUID id);
 }
