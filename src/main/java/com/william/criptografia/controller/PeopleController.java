056c7328b77e583564c609bac1a7ba352e972974
bobonimo111 <carlosmaycon443@gmail.com>
2025-07-09 14:14:46 -0300

    Coreção em rotas e coreção la logica do update

--- src/main/java/com/william/criptografia/controller/PeopleController.java
+++ src/main/java/com/william/criptografia/controller/PeopleController.java
@@ -1,15 +1,19 @@
 package com.william.criptografia.controller;
 
 import com.william.criptografia.dto.PeopleDto;
-// import com.william.criptografia.service.CriptografadorAES;
 import com.william.criptografia.service.PeopleService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.UnsupportedEncodingException;
+import java.security.InvalidKeyException;
 import java.util.UUID;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
 
 @RestController
 @RequestMapping("people")
@@ -18,52 +22,33 @@ public class PeopleController {
     @Autowired
     private PeopleService peopleService;
 
-    // @Autowired
-    // private CriptografadorAES cript;
-
     @PostMapping
     private ResponseEntity<?> createNew(@RequestBody PeopleDto req) {
         return peopleService.createNew(req);
     }
 
     @GetMapping
-    private ResponseEntity<?> get(@RequestParam(name = "uuid") UUID uuid) {
+    private ResponseEntity<?> get(@RequestParam(name = "id") UUID uuid) throws Exception {
         return peopleService.findOne(uuid);
     }
 
     @GetMapping("/all")
-    private ResponseEntity<?> getAll() {
+    private ResponseEntity<?> getAll(@RequestParam(defaultValue = "false",name = "criptografia") boolean isCriptografado) {
+        if (isCriptografado) {
+            return peopleService.findAllCriptografado();
+        }
         return peopleService.findAll();
     }
 
+
     @PutMapping
-    private ResponseEntity<?> edit(@RequestBody PeopleDto req) {
+    private ResponseEntity<?> edit(@RequestBody PeopleDto req)
+            throws IllegalBlockSizeException, UnsupportedEncodingException, BadPaddingException, InvalidKeyException {
         return peopleService.update(req);
     }
 
     @DeleteMapping
-    private ResponseEntity<?> remove(@RequestParam(name = "uuid") UUID uuid) {
+    private ResponseEntity<?> remove(@RequestParam(name = "id") UUID uuid) {
         return peopleService.remove(uuid);
     }
-
-    // @PostMapping("/cript")
-    // public ResponseEntity<?> postMethodEnc(@RequestParam(name = "text") String text) {
-    //     try {
-    //         return ResponseEntity.ok().body(cript.encriptyMessage(text));
-    //     } catch (Exception ex) {
-    //         System.out.println(ex.getMessage());
-    //         return ResponseEntity.badRequest().body(ex);
-    //     }
-    // }
-
-    // @PostMapping("/dec")
-    // public ResponseEntity<?> postMethodDec(@RequestParam(name = "text") String text) {
-    //     try {
-    //         return ResponseEntity.ok().body(cript.decryptMessage(text));
-    //     } catch (Exception ex) {
-    //         System.out.println(ex.getMessage());
-    //         return ResponseEntity.badRequest().body(ex);
-    //     }
-    // }
-
 }
--- src/main/java/com/william/criptografia/service/PeopleService.java
+++ src/main/java/com/william/criptografia/service/PeopleService.java
@@ -5,15 +5,18 @@ import com.william.criptografia.entity.PeopleEntity;
 import com.william.criptografia.repository.PeopleRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.io.UnsupportedEncodingException;
-import java.security.InvalidKeyException;
-import java.util.UUID;
-
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
+import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
+import java.security.InvalidKeyException;
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
 @Service
 public class PeopleService {
@@ -26,43 +29,121 @@ public class PeopleService {
 
     // Documente ID Social Security cards in united states or RG in Brazil
     public ResponseEntity<?> createNew(PeopleDto req) {
+        PeopleEntity entity;
         try {
             byte[] DocCrypt = cript.encriptyMessage(req.userDocument());
             byte[] cardTokenCript = cript.encriptyMessage(req.creditCardToken());
+            entity = PeopleEntity.builder()
+                    .userDocument(DocCrypt)
+                    .creditCardToken(cardTokenCript)
+                    .name(req.name())
+                    .build();
+            peopleRepository.save(entity);
         } catch (Exception ex) {
-
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex);
         }
 
-        PeopleEntity entity = PeopleEntity.builder()
-                .userDocument(null)
-                .creditCardToken(null)
-                .name(null)
-                .build();
-
-        return null;
+        return ResponseEntity.status(HttpStatus.OK).body(new PeopleDto(entity.getId(),
+                entity.getName(),
+                new String(entity.getUserDocument(), StandardCharsets.UTF_8),
+                new String(entity.getCreditCardToken(), StandardCharsets.UTF_8)
+        ));
     }
 
     public ResponseEntity<?> findAll() {
-        // Tenho de descriptografar
-        return null;
+
+        List<PeopleEntity> entitys = peopleRepository.findAll();
+        List<PeopleDto> peopleDtos = entitys
+                .stream()
+                .map(entity -> {
+                            try {
+                                return new PeopleDto(entity.getId(),
+                                        entity.getName(),
+                                        cript.decryptMessage(entity.getUserDocument()),
+                                        cript.decryptMessage(entity.getCreditCardToken()));
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                ).toList();
+
+        return ResponseEntity.status(HttpStatus.OK).body(peopleDtos);
     }
 
     public ResponseEntity<?> findAllCriptografado() {
         // So puxo tudo do jeito que está mesmo
-        return null;
+        List<PeopleEntity> entitys = peopleRepository.findAll();
+        List<PeopleDto> peopleDtos = entitys
+                .stream()
+                .map(entity -> {
+                            try {
+                                return new PeopleDto(entity.getId(),
+                                        entity.getName(),
+                                        new String(entity.getUserDocument(), StandardCharsets.UTF_8),
+                                        new String(entity.getCreditCardToken(), StandardCharsets.UTF_8));
+                            } catch (Exception e) {
+                                throw new RuntimeException(e);
+                            }
+                        }
+                ).toList();
+
+        return ResponseEntity.status(HttpStatus.OK).body(peopleDtos);
     }
 
-    public ResponseEntity<?> findOne(UUID uuid) {
-        // Tenho de descriptografar
-        return null;
+    public ResponseEntity<?> findOne(UUID uuid) throws Exception {
+        Optional<PeopleEntity> entityOp = peopleRepository.findById(uuid);
+
+        if (!entityOp.isPresent()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+        }
+
+        PeopleEntity entity = entityOp.get();
+
+        return ResponseEntity.status(HttpStatus.OK).body(new PeopleDto(entity.getId(),
+                entity.getName(),
+                cript.decryptMessage(entity.getUserDocument()),
+                cript.decryptMessage(entity.getCreditCardToken())));
     }
 
-    public ResponseEntity<?> update(PeopleDto req) {
+    public ResponseEntity<?> update(PeopleDto req) throws IllegalBlockSizeException, UnsupportedEncodingException, BadPaddingException, InvalidKeyException {
         // Tenho de criptografar
-        return null;
+        Optional<PeopleEntity> entityOp = peopleRepository.findById(req.id());
+
+        if (entityOp.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+        }
+        PeopleEntity entity = entityOp.get();
+
+        if (req.name() != null) {
+            entity.setName(req.name());
+        }
+
+        if (req.creditCardToken() != null) {
+            entity.setCreditCardToken(cript.encriptyMessage(req.creditCardToken()));
+        }
+
+        if (req.userDocument() != null) {
+            entity.setUserDocument(cript.encriptyMessage(req.userDocument()));
+        }
+
+        peopleRepository.save(entity);
+
+        return ResponseEntity.status(HttpStatus.OK)
+                .body(new PeopleDto(entity.getId(),
+                        entity.getName(),
+                        new String(entity.getUserDocument(), StandardCharsets.UTF_8),
+                        new String(entity.getCreditCardToken(), StandardCharsets.UTF_8)));
     }
 
     public ResponseEntity<?> remove(UUID uuid) {
-        return null;
+        Optional<PeopleEntity> entityOp = peopleRepository.findById(uuid);
+
+        if (entityOp.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NO_CONTENT).body(null);
+        }
+
+        peopleRepository.delete(entityOp.get());
+
+        return  ResponseEntity.status(HttpStatus.OK).body(null);
     }
 }
